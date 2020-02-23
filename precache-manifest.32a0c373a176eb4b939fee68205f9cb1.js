653b599c2ac86dd4425838968f069eb2387465ab
Juan David <gottygarcia@gmail.com>
2020-02-23 17:38:57 -0500

    Updates

---
+++ precache-manifest.32a0c373a176eb4b939fee68205f9cb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5995bd847322df84afa6f2d236aee08c",
+    "url": "/react-classic-vs-hooks/index.html"
+  },
+  {
+    "revision": "be6bd5107e3bcbcc3f72",
+    "url": "/react-classic-vs-hooks/static/css/main.4dbca242.chunk.css"
+  },
+  {
+    "revision": "5cdd4c76c288c32322ac",
+    "url": "/react-classic-vs-hooks/static/js/2.d446f3a8.chunk.js"
+  },
+  {
+    "revision": "ece57687f0b2d7fbf4a41a327fd051ba",
+    "url": "/react-classic-vs-hooks/static/js/2.d446f3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be6bd5107e3bcbcc3f72",
+    "url": "/react-classic-vs-hooks/static/js/main.b88d3383.chunk.js"
+  },
+  {
+    "revision": "45124c598ddec22af48e",
+    "url": "/react-classic-vs-hooks/static/js/runtime-main.7d1813d1.js"
+  }
+]);
